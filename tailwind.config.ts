071c154fc5369692bb0d38e40bcc762c6d248583
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-02-28 10:29:12 -0500

    build: add tailwind screensizes and font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    fontSize: {
+      body2: "0.8rem",
+      body1: "1rem",
+      h6: "1rem",
+      h5: "1.25rem",
+      h4: "1.563rem",
+      h3: "1.953rem",
+      h2: "2.441rem",
+      h1: "3.052rem",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
